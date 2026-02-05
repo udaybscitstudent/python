56d791a7864f0b6925eb6f0559cf9cba9680581d
Uday kumar <udaykumarraj2058@gmail.com>
2026-02-05 14:04:08 +0530

    add indent and separators in this file

--- python/test_json.py
+++ python/test_json.py
@@ -6,6 +6,7 @@ The json module can convert Python objects into JSON strings and vice versa.
 """
 '''
 import json
+# convert python object to json string
 
 student = {
     "name" : "uday",
@@ -14,7 +15,25 @@ student = {
     "mobile" :9334612395,
     "email" : "uday@gmail.com"
 }
-
-y = json.dumps(student)
-
+y = json.dumps(student, indent=4 , separators=(". ", " = "), sort_keys=True)
+print(type(y))
 print(y)
+
+# convert json string to python object
+
+json_string = '{"name": "uday", "roll": 18, "course": "bscit", "mobile": 9334612395, "email": "uday@gmail.com"}'
+student_dict = json.loads(json_string)
+print(type(student_dict))
+print(student_dict)
+
+# you can convert python object to json string 
+print(json.dumps({"name":"uday", "roll" : 18}))
+print(json.dumps([1, 2, 3, 4, 5]))
+print(json.dumps(("apple", "banana", "cherry")))
+print(json.dumps("Hello World"))
+print(json.dumps(42))
+print(json.dumps(3.14))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
